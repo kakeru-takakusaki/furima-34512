6d5fcaf8ae46dd923585466008df28ae5db3c68e
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 18:03:26 +0900

    category_idを使用するためのActiveHashの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+  end
+
 end
