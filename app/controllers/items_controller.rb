a162e386c56a537f9d5f7e54bda177d8a7fef102
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 21:30:55 +0900

    item/createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
