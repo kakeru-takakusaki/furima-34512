f6a5a6068ba809ddcfac94ea9ede7da67807ddeb
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 18:17:24 +0900

    通常カラムのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+  end
+
   with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
   end
