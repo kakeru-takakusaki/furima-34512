ab575f1dfb37ee9ac7584b40185bff8e75fadc38
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-03 01:01:22 +0900

    itemモデルの単体テストコードで使用するimageの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image {Faker::Avatar.image}
     name {Faker::Name.name}
     text {Faker::Types.rb_string}
     category_id {Faker::Number.between(from: 2, to: 11)}
@@ -9,5 +8,9 @@ FactoryBot.define do
     prefecture_id {Faker::Number.between(from: 2, to: 48)}
     shipping_day_id {Faker::Number.between(from: 2, to: 4)}
     price {Faker::Number.between(from: 300, to: 9999999)}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
