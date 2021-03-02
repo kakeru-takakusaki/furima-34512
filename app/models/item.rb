4da18fd7af1bdd6df40ca386aa456a3c02c7e3c7
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 21:30:15 +0900

    imageのバリデーションとエラーメッセージの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :text
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10000000 }, format: { with: /\A[0-9]+\z/, message: 'には半角数字のみで入力してください' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 10000000, message: 'の範囲外です' }, format: { with: /\A[0-9]+\z/, message: 'には半角数字のみで入力してください' }
   end
 
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options presence: true, numericality: { other_than: 1, message: 'を選択してください' } do
     validates :category_id
     validates :status_id
     validates :shipping_fee_id
