fe68f61dde7e546d9eb203b9367e88b7ce23f2b0
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 21:47:35 +0900

    FactoryBotの定義（Itemモデルの単体テストコードで使用）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Avatar.image}
+    name {Faker::Name.name}
+    text {Faker::Types.rb_string}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    status_id {Faker::Number.between(from: 2, to: 7)}
+    shipping_fee_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    shipping_day_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
   end
 end
