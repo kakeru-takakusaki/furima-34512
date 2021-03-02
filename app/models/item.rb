32d3405e132dd6ca3f6214828a022c7eb4b07557
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-02 18:35:29 +0900

    ActiveHash　prefectureの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_fee
+  belongs_to :prefecture
 
   with_options presence: true do
     validates :name
@@ -15,6 +16,7 @@ class Item < ApplicationRecord
     validates :category_id
     validates :status_id
     validates :shipping_fee_id
+    validates :prefecture_id
   end
 
 end
