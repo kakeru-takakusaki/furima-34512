da1661ae440ead41d1c04635e80ee58fa49a207b
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-03 17:09:42 +0900

    販売価格は半角数字のみ保存可能であることのテストコードをより詳細に記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,9 +69,20 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price の範囲外です')
       end
-      it '販売価格は半角数字のみ保存可能であること' do
+      it '販売価格は半角数字のみ保存可能であること(全角文字では保存できないこと)' do
         @item.price = '５０００'
         @item.valid?
+        expect(@item.errors.full_messages).to include('Price の範囲外です')
+      end
+      it '販売価格は半角数字のみ保存可能であること(半角英数字混合では保存できないこと)' do
+        @item.price = 'five1000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price の範囲外です')
+      end
+      it '販売価格は半角数字のみ保存可能であること(半角英字のみで保存できないこと)' do
+        @item.price = 'five thousand'
+        @item.valid?
+        binding.pry
         expect(@item.errors.full_messages).to include('Price の範囲外です')
       end
     end
