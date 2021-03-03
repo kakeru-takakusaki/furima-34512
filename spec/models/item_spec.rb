78cda4ef93698c575bca828371e77a002c8e261e
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-03 16:19:43 +0900

    binding.pryの削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,6 @@ RSpec.describe Item, type: :model do
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '５０００'
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include('Price の範囲外です')
       end
     end
