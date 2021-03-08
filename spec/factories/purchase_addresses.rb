58d79339341d0c78ccb42d2a8e55e32fe43f01d5
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-08 17:49:53 +0900

    PurchaseAddressのテストコードで使用するFactoryBotの定義

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_address do
+    postal_code    { '123-4567' }
+    prefecture_id  { Faker::Number.between(from: 2, to: 48) }
+    municipality   { '東京都' }
+    address        { '1-1' }
+    building_name  { '東京ハイツ' }
+    phone_number   { '09012345678' }
+  end
+end
