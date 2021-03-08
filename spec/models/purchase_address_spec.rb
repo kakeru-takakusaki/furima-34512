1a5362011aca269cc0f3bdd9385ffb8b2a8d6962
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-08 19:40:41 +0900

    異常系テストの記述とsleep

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe PurchaseAddress, type: :model do
     user = FactoryBot.create(:user)
     item = FactoryBot.create(:item)
     @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
+    sleep 0.1
   end
   describe '商品購入の情報保存' do
     context '保存できる場合' do
@@ -18,8 +19,55 @@ RSpec.describe PurchaseAddress, type: :model do
     end
 
     context '保存できない場合' do
-      it '' do
-        
+      it 'user_idがなくてはならないこと' do
+        @purchase_address.user_id = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idがなくてはならないこと' do
+        @purchase_address.item_id = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it '配送先の情報として、郵便番号が必須であること' do
+        @purchase_address.postal_code = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号の保存にはハイフンが必要であること' do
+        @purchase_address.postal_code = '1234567'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '配送先の情報として、都道府県が必須であること' do
+        @purchase_address.prefecture_id = 1
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Prefecture を選択してください")
+      end
+      it '配送先の情報として、市区町村が必須であること' do
+        @purchase_address.municipality = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '配送先の情報として、番地が必須であること' do
+        @purchase_address.address = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it '配送先の情報として、電話番号が必須であること' do
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号は11桁以内の数値のみ保存可能なこと' do
+        @purchase_address.phone_number = '090-1234-5678'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号は半角の数値のみ保存可能なこと' do
+        @purchase_address.phone_number = '０９０１２３４５６７８'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
       end
     end
   end
