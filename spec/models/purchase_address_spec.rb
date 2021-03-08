0ad06390fac04a0d3644b22ce8521cd7afe2a2a4
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-08 17:51:11 +0900

    PurchaseAddressの単体テストコード（before doと正常系テスト）

---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
+  end
+  describe '商品購入の情報保存' do
+    context '保存できる場合' do
+      it 'ログイン状態の出品者以外のユーザーのみ、必要な情報を適切に入力すると、商品の購入ができること' do
+        expect(@purchase_address).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it '' do
+        
+      end
+    end
+  end
+end
