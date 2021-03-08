3523dd8f5af5618ebe1ec2738550865354312769
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-08 15:18:07 +0900

    purchase_idのバリデーションを削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -10,11 +10,10 @@ class PurchaseAddress
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A[0-9]+\z/ }
-    validates :purchase_id
   end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
