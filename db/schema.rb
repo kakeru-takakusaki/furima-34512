f099e0676536f781c1f147b13fe69b887f7ae448
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-10 23:38:26 +0900

    sold_statusカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_110707) do
+ActiveRecord::Schema.define(version: 2021_03_10_120837) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_110707) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "sold_status", default: "sale", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
