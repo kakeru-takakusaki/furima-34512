a78ce0bfd69692a5ca84d0aa1be270c953f306e1
kakeru-takakusaki <78397875+kakeru-takakusaki@users.noreply.github.com>
2021-03-11 12:17:55 +0900

    出品時にsold_statusを受け取れるように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :sold_status).merge(user_id: current_user.id)
   end
 
   def redirect_root
